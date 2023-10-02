1a7322da2d2f7f8dd7ac3f82004f6a7d11923e54
siondawson <>
2023-10-02 11:08:30 +0000

    add allauth templates and base template

--- boutqiue_ado/settings.py
+++ boutqiue_ado/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'boutqiue_ado.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
